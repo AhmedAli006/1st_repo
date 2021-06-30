fe44af6fef25e5dce103544a11e73162351cfa10
ahmedali762 <ahmed.spidy444@gmail.com>
2021-06-30 15:15:38 +0500

    1st_rep

---
+++ app.js
@@ -0,0 +1,218 @@
+//                                                               assignment # 1
+
+// alert('JavaScript Alert')
+
+// alert('welcome to js land..')
+
+//                                                               assignment # 2
+
+//                                                               assignment # 3
+
+// var age = prompt('enter Age')
+// var year = prompt('enter Birth Year')
+
+// document.write('your age is ',age)
+// document.write(' your birth year is ',year)
+
+
+
+
+
+//                                                               assignment # 4
+
+// 
+// var 1apple
+// var sign.
+// var -type
+// 
+// 
+// var ABCD
+// var $name
+// var ahmed_ali
+// 
+
+
+//                                                               assignment # 5
+
+// var a = 10
+// var b = 5
+
+
+//  c = a + b
+//  e = a - b
+//  f = a * b
+//  g = a / b
+// h = (a / 2) / 5
+
+// console.log(c)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+
+// console.log(++a)
+// console.log(++a + ++b - --b)
+
+// console.log(c)
+
+//                        ticket
+
+// var MovieTicket = 600
+
+// var totalno = prompt('tickets cost 600 how many do you want');
+
+//  document.write('cost of ticket is 600')
+
+//  if (totalno >= 1) {
+
+//       total = 600 * totalno
+//  }
+
+// document.write(' and your total is ',total)
+
+//       table
+
+// var a =  prompt('')
+// for(var i=1 ; i>20 ; i++){
+    
+//    console.log( a + 'X ' + i + '=' + a*i )
+    
+// }
+
+// document.write('4 X 2 = ', c )
+
+
+//       temperatue formula
+
+// var celsius  = + prompt('convert to Fahrenheit')
+
+// Fahrenheit = (celsius * 9/5 ) + 32
+
+// document.write(  Fahrenheit )
+
+// document.write("<br>");
+
+
+// var Fahrenheit = + prompt( 'convert to celsius')
+
+// celsius = ( Fahrenheit - 32) * 5/9
+
+// document.write(  celsius  )
+
+//    shoping list
+
+// var item1 = 600
+// var item2 = 250
+// var dcharges = 100
+
+// var q1 = + prompt('quantity of item1')
+// var q2 = + prompt('quantity of item2')
+
+//   a = q1 * item1
+//   b = q2 * item2
+
+// document.write(' price of item 1 =',item1)
+// document.write("<br>");
+// document.write(' quantity of item 1 =',q1)
+// document.write("<br>");
+// document.write(' price of item 2 =',item2)
+// document.write("<br>");
+// document.write(' quantity of item 2 =',q2)
+// document.write("<br>");
+// document.write('delivery charges = 100')
+// document.write("<br>");
+// document.write("<br>");
+
+// total = item1 + item2 + dcharges
+// document.write('total cost of your order is = ',total)
+
+//  marksheet
+
+// var bio = + prompt('boilogy numbers')
+// var phy = + prompt('physics numbers')
+// var chem = + prompt('chemistry numbers')
+// var urdu = + prompt('urdu numbers')
+// var eng = + prompt('english numbers')
+// var isl = + prompt('islamiat numbers')
+
+// var total = 550
+// var marks = bio+chem+phy+urdu+eng+isl
+
+// var per =(marks / total) * 100
+
+// document.write( 'Your total marks are '+ '= ' + marks + '<br>')
+
+
+// document.write( 'Your percentage is ' + '= ' + per +'%')
+
+
+
+//        dollar
+
+// var dollar = prompt('enter PKR to convert ')
+
+// total = dollar * 160
+
+
+// document.write('total currency in dollar =',dollar)
+
+// document.write("<br>");
+
+// document.write('total currency in PKR =',total)
+
+
+//  age calculator
+// var currntYear = prompt('enter current year ')
+// var birthYear = prompt('enter birth year')
+
+// var age = currntYear - birthYear
+
+// document.write( 'you are ' + age + ' years old')
+
+//   the geometrizer
+
+// var raidus = prompt('enter radius')
+// var circumference = (2 * 3.14) * radius
+// var area = 3.14 * (raidus * raidus )
+
+// document.write('the radius of circle' + raidus)
+// document.write('the circumference is ' + circumference)
+// document.write('the area is' + area)
+
+
+//     life time supply
+
+// var snack = prompt('your fav snack')
+// var amount = prompt('amount of snack')
+// var age = prompt('your age')
+// var life = prompt('your estimated max age')
+
+// var x = age * 365 + amount
+
+// console.log('you will need ' + x + ' to last you ' + age + ' years' )
+
+//                                                               assignment # 6-9
+// var a = 2, b = 1
+// var result = --a - --b + ++b + b--
+
+// console.log(result)
+
+//                                                               
+//                                                               assignment # 14-16
+
+var city = prompt('enter city')
+
+var gender = prompt('enter gender')
+
+if ((city == 'karachi') && (gender='male')) {
+    document.write('wellcome sir')
+    document.write('<br>')
+    document.write('Welcome to the city of lights')
+}else( gender='female')
+{
+    document.write('wellcome madam')
+    document.write('<br>')
+    document.write('Welcome to the city of lights')
+}
+
+
